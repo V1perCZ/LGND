fc21de05d655a65ff2991db6cda19e408790737d
Lukáš Müller <61428713+V1perCZ@users.noreply.github.com>
2020-02-24 19:54:20 +0100

    Zatčení
    Lua

--- __resource.lua
+++ __resource.lua
@@ -1,13 +1,15 @@
--- Manifest
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_script 'cl_delivery_gopostal.lua'
-client_script 'cl_delivery_china.lua'
-client_script 'cl_delivery_pizza.lua'
-client_script 'cl_delivery_weed.lua'
-client_script 'cl_delivery_meth.lua'
+description 'esx_ktackle - enables tackling for cops'
 
+version '1.0.0'
 
-server_script 
-'sv_delivery.lua'
-'@mysql-async/lib/MySQL.lua'
+server_scripts {
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
